6087854cb1d413d3c96db1a8dd11a14d241bbba6
hirocaster <hohtsuka@gmail.com>
2013-04-30 08:46:10 +0900

    Add UINavigationController

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -9,20 +9,21 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window .makeKeyAndVisible
 
-    @window .rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    @window .rootViewController = UINavigationController.alloc.initWithRootViewController(controller)
 
-    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
-    @blue_view .backgroundColor = UIColor.blueColor
-    @window .addSubview(@blue_view)
+    # @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    # @blue_view .backgroundColor = UIColor.blueColor
+    # @window .addSubview(@blue_view)
 
-    @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
-    @green_view .backgroundColor = UIColor.greenColor
-    @window .addSubview(@green_view)
+    # @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+    # @green_view .backgroundColor = UIColor.greenColor
+    # @window .addSubview(@green_view)
 
-    @red_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
-    @red_view .backgroundColor = UIColor.redColor
+    # @red_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+    # @red_view .backgroundColor = UIColor.redColor
 
-    @blue_view .addSubview(@red_view)
+    # @blue_view .addSubview(@red_view)
 
     true
   end
